77eb4fb2d3519422390f7ecf705da05eed19242a
nasii05 <iamnasih@gmail.com>
2025-04-08 10:15:23 +0530

    authorize middleware

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,13 @@
 import {Router} from 'express';
+import authorize from '../middlewares/auth.middleware.js';
 import {getUser, getUsers} from "../controllers/user.controller.js";
+import errorMiddleware from "../middlewares/error.middleware.js";
 
 const userRouter = Router();
 
 userRouter.get('/', getUsers);
 
-userRouter.get('/:id', getUser);
+userRouter.get('/:id', authorize , errorMiddleware,getUser);
 
 userRouter.post("/", (req, res)=>{
     res.send({title: "CREATE new user"})
